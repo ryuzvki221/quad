3b273b7d7f0465cea7296354dede6209a070a3e0
ryuzvki-sn <contact.stateless@gmail.com>
2023-02-27 10:34:44 +0000

    piscine quad

---
+++ test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+func main() {
+
+	fmt.Println("+-------Quad A*-------+")
+	piscine.QuadA(5, 3)
+	fmt.Println("+-------Quad B*-------+")
+	piscine.QuadB(5, 3)
+	fmt.Println("+-------Quad C*-------+")
+	piscine.QuadC(5, 3)
+	fmt.Println("+-------Quad D*-------+")
+	piscine.QuadD(5, 3)
+	fmt.Println("+-------Quad E*-------+")
+	piscine.QuadE(5, 3)
+}
